292fe7ffcf35304a297799a536f8fe4fe3291b25
Mason-DeMelo <mason.demelo@uconn.edu>
2016-01-07 13:20:56 -0500

    Attempt at playing a song
    includes a defective update to momar.py as well as an mp3 file for
    testing purposes.

--- momar.py
+++ momar.py
@@ -7,6 +7,14 @@ except ImportError:
     # for Python3
     from tkinter import *
 
+import winsound
+import tkSnack
+
+#AUDIO PLAYER
+def playPause():
+    song = open("testSong.mp3").read()
+    winsound.PlaySound(song, winsound.SND_MEMORY)
+
 momarInstance = Tk()
 momarInstance.title("MOMAR Music")
 momarInstance.geometry("200x75")
@@ -19,11 +27,14 @@ app.grid()
 backB= Button(app, text = "Back")
 backB.grid()
 #Play button
-playB= Button(app, text = "Play")
+playB= Button(app, text = "Play", command = playPause)
 playB.grid()
 #Skip Button
 skipB=Button(app, text = "Skip")
 skipB.grid()
 
 
+
+
 momarInstance.mainloop()
+
