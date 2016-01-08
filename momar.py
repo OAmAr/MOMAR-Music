07f92fba4434c7fe2b96f7362e064d9691a07900
Omar Amer <omar.amer@uconn.edu>
2016-01-08 17:13:27 -0500

    Clean up code

--- momar.py
+++ momar.py
@@ -5,22 +5,43 @@ try:
 except ImportError:
     # for Python3
     from tkinter import *
+    
+import tkinter.messagebox
+import time 
 
 #Music Player Things
 music = pyglet.resource.media('testSong.mp3')
 player = pyglet.media.Player()
 
 player.queue(music)
-import tkinter.messagebox
+
 def toggleState():
     if player.playing:
         player.pause()
     else:
         player.play()
+        return
+
+def playcount():
+    while player.playing:
+        print('playing')
+        app.time.set(player.time)
+        time.sleep(1.0)
+        if player.time == music.duration:
+            app.time.set(0)
+            #and move to next song
+def playwait():
+    while not player.playing:
+        print(
+            'waiting')
+    playcount()
+            
+        
+            
 
 #Window Things
 
-class Player (Frame):
+class PlayWindow (Frame):
     """Main Window"""
     def __init__(self, master):
         Frame.__init__(self, master)
@@ -46,7 +67,7 @@ class Player (Frame):
         self.vol.grid(row = 0, column = 1, rowspan = 3, sticky = W)
 
         """Time Scrub"""
-        self.time = Scale(self, from_= music.duration, to = 0,
+        self.time = Scale(self, to= music.duration, from_ = 0,
                           orient = HORIZONTAL , command = self.timechanged)
         #when song ends this needs to change, use config?
 
@@ -66,12 +87,10 @@ class Player (Frame):
 
     def volchanged(self, event):
         player.volume= float( self.vol.get())/100
-        print (player.volume)
 
     def timechanged(self, event):
         player.seek(self.time.get() )
-        
-    
+            
     def placeholder(self, text):
         messagebox.showerror(title = "no code",
                            message = "Oops! There's no code here! "
@@ -85,6 +104,7 @@ if __name__ == "__main__":
     root.title("MOMAR")
     root.geometry("200x103")
     
-    app = Player(root)
-
+    app = PlayWindow(root)
+    playwait()
     root.mainloop()
+    
